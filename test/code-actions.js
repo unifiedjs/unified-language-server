356be98f63383e2494c0c2a2276f69b3f0225ce5
Remco Haszing <remcohaszing@gmail.com>
2022-03-28 17:02:10 +0200

    Refactor to improve tests
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

---
+++ test/code-actions.js
@@ -0,0 +1,34 @@
+import {createUnifiedLanguageServer} from '../index.js'
+
+createUnifiedLanguageServer({
+  processorName: 'remark',
+  processorSpecifier: 'remark',
+  plugins: [warn]
+})
+
+/** @type {import('unified').Plugin<Array<void>>} */
+function warn() {
+  return (_, file) => {
+    // Insert
+    file.message('', {line: 1, column: 1}).expected = ['insert me']
+
+    // Replace
+    file.message('', {
+      start: {line: 1, column: 1},
+      end: {line: 1, column: 7}
+    }).expected = ['replacement']
+
+    // Delete
+    file.message('', {
+      start: {line: 1, column: 1},
+      end: {line: 1, column: 7}
+    }).expected = ['']
+
+    // @ts-expect-error We are deliberately testing invalid types here, because
+    // the expected field used to be untyped for a long time.
+    file.message('', {line: 1, column: 1}).expected = 'insert me'
+    // @ts-expect-error
+    file.message('', {line: 1, column: 1}).expected = [12]
+    file.message('', {line: 1, column: 1})
+  }
+}
