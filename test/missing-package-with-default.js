752400aea262aea2369e9182ef8a34f832e134b0
Remco Haszing <remcohaszing@gmail.com>
2023-08-22 11:38:23 +0200

    Update dependencies (#57)
    This makes it compatible with unified 11 and unified-engine 11.

--- test/missing-package-with-default.js
+++ test/missing-package-with-default.js
@@ -3,5 +3,6 @@ import {createUnifiedLanguageServer} from 'unified-language-server'
 
 createUnifiedLanguageServer({
   processorName: 'xxx-missing-yyy',
+  // @ts-expect-error This will be ok when we update to remark 15.
   defaultProcessor: remark
 })
