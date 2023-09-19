89f86d53eb9e107e743b9efaaed608c12b8dc294
Remco Haszing <remcohaszing@gmail.com>
2023-09-19 10:48:00 +0200

    Update to remark 15 and remark-cli 12

--- test/missing-package-with-default.js
+++ test/missing-package-with-default.js
@@ -4,6 +4,5 @@ import {createUnifiedLanguageServer} from 'unified-language-server'
 createUnifiedLanguageServer({
   configurationSection: 'xxx-missing-yyy',
   processorName: 'xxx-missing-yyy',
-  // @ts-expect-error This will be ok when we update to remark 15.
   defaultProcessor: remark
 })
